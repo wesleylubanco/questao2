d576d52f3c06ca94226c739692708321fd1163e9
wesleylubanco <wesleylubanco@hotmail.com>
2014-02-22 11:23:21 -0300

    cliente

--- cliente.py
+++ cliente.py
@@ -1,10 +1,1 @@
-import socket
-HOST = '192.168.1.171'              # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-orig = (HOST, PORT)
-udp.bind(orig)
-while True:
-    msg, cliente = udp.recvfrom(1024)
-    print cliente, msg
-udp.close()
+while
